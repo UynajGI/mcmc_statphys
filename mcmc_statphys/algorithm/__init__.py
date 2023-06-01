73cc3937e24cdc6b46cdff2e0a313a834a2d3d42
UynajGI <suquan12148@outlook.com>
2023-06-01 12:13:36 +0800

    feat: 🎸 新增Demon方法
    BREAKING CHANGE: 🧨 Demon方法！

--- mcmc_statphys/algorithm/__init__.py
+++ mcmc_statphys/algorithm/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ['Metropolis', 'Wolff', 'Anneal', 'Parallel', 'WangLandau']
+__all__ = ['Metropolis', 'Wolff', 'Anneal', 'Parallel', 'WangLandau', 'Demon']
 
 from .Metropolis import Metropolis
 from .Wolff import Wolff
 from .Anneal import Anneal
 from .Parallel import Parallel
 from .WangLandau import WangLandau
+from .Demon import Demon
