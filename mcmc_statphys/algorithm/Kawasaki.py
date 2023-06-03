27bec708cc665f0c4314ddb698d4b8864e4583a7
UynajGI <suquan12148@outlook.com>
2023-06-03 21:15:37 +0800

    perf: ⚡️ 优化kawasaki采样速度

--- mcmc_statphys/algorithm/Kawasaki.py
+++ mcmc_statphys/algorithm/Kawasaki.py
@@ -36,16 +36,15 @@ class Kawasaki(Metropolis):
 
     def iter_sample(self, T: float, uid: str = None, ac_from='class') -> str:
         uid = self._setup_uid(uid)
-        site = tuple(np.random.randint(0, self.model.L, size=self.model.dim))
-        site2 = tuple(np.random.randint(0, self.model.L, size=self.model.dim))
-        # 若两个 site 的 spin 相同，则重新选取
-        while self.model.spin[site] == self.model.spin[site2]:
-            site2 = tuple(
-                np.random.randint(0, self.model.L, size=self.model.dim))
-        temp_model = copy.deepcopy(self.model)
-        delta_E = self.model._change_delta_energy(site)
-        delta_E += self.model._change_delta_energy(site2)
-        if not _sample_acceptance(delta_E, T, form=ac_from):
-            self.model = temp_model
+        _plus = np.argwhere(self.model.spin == 1)
+        _minus = np.argwhere(self.model.spin == -1)
+        # 从 plus 中随机选取一个
+        _site = _plus[np.random.choice(np.arange(len(_plus)))]
+        _site2 = _minus[np.random.choice(np.arange(len(_minus)))]
+        _temp_model = copy.deepcopy(self.model)
+        _delta_E = self.model._change_delta_energy(_site)
+        _delta_E += self.model._change_delta_energy(_site2)
+        if not _sample_acceptance(_delta_E, T, form=ac_from):
+            self.model = _temp_model
         self._save_date(T, uid)
         return uid
