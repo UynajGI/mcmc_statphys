8a34265263eddecd9e79eace9f312274c7c3c2d4
UynajGI <suquan12148@outlook.com>
2023-06-03 21:58:34 +0800

    feat: 🎸 新增RFising类
    BREAKING CHANGE: 🧨 RFising类！

--- mcmc_statphys/model/__init__.py
+++ mcmc_statphys/model/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ["Ising", "Heisenberg", "XY", "Potts", "SKmodel"]
+__all__ = ["Ising", "Heisenberg", "XY", "Potts", "SKmodel", "RFIsing"]
 
 from .Ising import Ising
 from .Heisenberg import Heisenberg
 from .XY import XY
 from .Potts import Potts
 from .SKmodel import SKmodel
+from .RFIsing import RFIsing
