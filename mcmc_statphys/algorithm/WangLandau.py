f4c719c60a49ea78d63d0ce8f17604e8aa7e002a
UynajGI <suquan12148@outlook.com>
2023-06-03 23:25:46 +0800

    perf: ⚡️ 修改了wanglandau算法的逻辑

--- mcmc_statphys/algorithm/WangLandau.py
+++ mcmc_statphys/algorithm/WangLandau.py
@@ -11,67 +11,100 @@ class WangLandau():
     ==========
     WangLandau is the Wang-Landau algorithm
     """
-    def __init__(self, model, glen=10):
+
+    def __init__(self, model, gspace=10):
         self.model = model
         self.name = "WangLandau"
-        self.logG = np.log(np.ones(glen))
-        self.hist = np.zeros(glen)
-        self.elst = np.linspace(0, self.model.maxenergy, glen)
+        self.elst = []
+        self.logG = []
+        self.hist = []
         self.logF = 1
+        self.gspace = gspace
 
     def _flat(self, array, epsilon=0.8):
-        return min(array[array > 0]) > epsilon * np.mean(array[array > 0])
+        nparray = np.array(array)
+        return min(
+            nparray[nparray > 0]) > epsilon * np.mean(nparray[nparray > 0])
 
     def sample(self, epsilon=1e-8):
         count = 0
-        total = int(np.log(epsilon) / np.log(0.5))
+        total = int(np.log(epsilon) / np.log(0.5)) + 1
         with tqdm(total=total) as pbar:
             while self.logF > epsilon:
-                site = tuple(
-                    np.random.randint(0, self.model.L, size=self.model.dim))
-                temp_model = copy.deepcopy(self.model)
-                self.model._change_delta_energy(site)
-                index_old = np.argmin(np.abs(self.elst - temp_model.energy))
-                index_new = np.argmin(np.abs(self.elst - self.model.energy))
-                if not np.log(np.random.rand()
-                              ) < self.logG[index_old] - self.logG[index_new]:
-                    self.model = temp_model
-                    index = index_old
+                if not self.elst:
+                    self.elst.append(self.model.energy)
+                    self.logG.append(1)
+                    self.hist.append(1)
                 else:
-                    index = index_new
-                self.hist[index] += 1
-                self.logG[index] += self.logF
+                    site = tuple(
+                        np.random.randint(0, self.model.L,
+                                          size=self.model.dim))
+                    temp_model = copy.deepcopy(self.model)
+                    self.model._change_delta_energy(site)
+                    index_old = np.argmin(np.abs(self.elst -
+                                                 temp_model.energy))
+                    index_new = np.argmin(np.abs(self.elst -
+                                                 self.model.energy))
+                    # self.elst[index_new] 是离当前能量太远
+                    if not np.log(np.random.rand(
+                    )) < self.logG[index_old] - self.logG[index_new]:
+                        self.model = temp_model
+                        index = index_old
+                    else:
+                        index = index_new
+
+                    if abs(self.elst[index] - self.model.energy) > self.gspace:
+                        if self.elst[index] - self.model.energy > 0:
+                            self.elst = self.elst[:index] + [
+                                self.model.energy
+                            ] + self.elst[index:]
+                            self.logG = self.logG[:index] + [
+                                self.logF
+                            ] + self.logG[index:]
+                            self.hist = self.hist[:index] + [
+                                1
+                            ] + self.hist[index:]
+                        else:
+                            self.elst = self.elst[:index + 1] + [
+                                self.model.energy
+                            ] + self.elst[index + 1:]
+                            self.logG = self.logG[:index + 1] + [
+                                self.logF
+                            ] + self.logG[index + 1:]
+                            self.hist = self.hist[:index + 1] + [
+                                1
+                            ] + self.hist[index + 1:]
+                    else:
+                        self.hist[index] += 1
+                        self.logG[index] += self.logF
                 count += 1
                 if count % 1000 == 0:
+                    narray = np.array(self.hist)
                     pbar.set_description(
                         "Now count: {c}e3; The logF is {f}; Min_h is {m}; 80% mean is {e}"
                         .format(c=count // 1000,
                                 f=self.logF,
-                                m=min(self.hist[self.hist > 0]),
-                                e=np.round(
-                                    0.8 * np.mean(self.hist[self.hist > 0]),
-                                    3)))
+                                m=min(narray[narray > 0]),
+                                e=np.round(0.8 * np.mean(narray[narray > 0]),
+                                           3)))
                 if self._flat(self.hist) and count > 1e4:
-                    self.hist = np.zeros(len(self.hist))
+                    self.hist = np.zeros(len(self.hist)).tolist()
                     self.logF /= 2
                     pbar.update(1)
                     count = 0
+                    self.logG = np.array(self.logG)
                     self.logG -= min(self.logG)
+                    self.logG = self.logG.tolist()
         return self.logG
 
-    def _log_partition(self, T):
-        logZ = 0
-        beta_lst = self.elst * (1 / T)
-        z = self.logG - beta_lst
-        logZ = np.log(np.sum(np.exp(z)))
-        return logZ
+    def energy(self, T):
+        elst = np.array(self.elst)
+        logG = np.array(self.logG)
+        return np.dot(elst, np.exp(logG - 1 / T * elst)) / np.sum(
+            np.exp(logG - 1 / T * elst))
 
-    def energy(self, T, epsilon=1e-8):
-        U = -T**2 * (self._log_partition(T + epsilon) -
-                     self._log_partition(T - epsilon)) / (2 * epsilon)
-        return U
-
-    def heatcap(self, T, epsilon=1e-8):
-        C = (self.energy(T + epsilon, epsilon=epsilon) -
-             self.energy(T - epsilon, epsilon=epsilon)) / (2 * epsilon)
-        return C
+    def heat(self, T):
+        elst = np.array(self.elst)
+        logG = np.array(self.logG)
+        return ((np.dot(elst**2, np.exp(logG - 1 / T * elst))) /
+                np.sum(np.exp(logG - 1 / T * elst)) - self.energy(T)**2) / T**2
